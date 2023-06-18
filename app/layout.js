283a846a708562e57d4520e5bc13f2a719b39d6a
mdazlaanzubair <mdazlaan1996@gmail.com>
2023-06-19 01:32:58 +0500

    cursor spotlight effect styled

--- app/layout.js
+++ app/layout.js
@@ -14,7 +14,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" data-theme="night" className="bg-navy-dark">
+    <html lang="en" data-theme="night" className="bg-navy-dark overflow-x-hidden">
       <body className={montserrat.className}>{children}</body>
     </html>
   );
