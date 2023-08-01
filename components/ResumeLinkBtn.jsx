fc4223589890f1e4cc4308f7a5dfb74f83645b0b
mdazlaanzubair <mdazlaan1996@gmail.com>
2023-08-01 20:07:57 +0500

    top nav animation completed

--- components/ResumeLinkBtn.jsx
+++ components/ResumeLinkBtn.jsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import Link from "next/link";
 
 const ResumeLinkBtn = () => {
@@ -5,9 +6,15 @@ const ResumeLinkBtn = () => {
     <Link
       href="https://rxresu.me/mdazlaan1996/resume-mdazlaanzubair"
       target="_blank"
-      className="btn btn-outline btn-primary rounded-sm capitalize hover:translate-x-1 hover:translate-y-1 transition-all ease-in-out duration-300"
     >
-      Resume
+      <motion.span
+        initial={{ opacity: 0, y: -10, scale: 0 }}
+        animate={{ opacity: 1, y: 0, scale: 1 }}
+        transition={{ duration: 0.2, delay: 0.5 }}
+        className="btn btn-outline btn-primary rounded-sm capitalize hover:translate-x-1 hover:translate-y-1 transition-all ease-in-out duration-300"
+      >
+        Resume
+      </motion.span>
     </Link>
   );
 };
--- components/navigation/TopNav.jsx
+++ components/navigation/TopNav.jsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { motion } from "framer-motion";
+
 import { useSocialContext } from "@/context/SocialContext";
 import Link from "next/link";
 import * as FontAwesome6Icon from "react-icons/fa";
@@ -10,14 +12,45 @@ const TopNav = () => {
   const [activeSocialLink, setActiveSocialLink] = useState("Available");
   const { socialLinks } = useSocialContext();
 
+  const navBarVariant = {
+    hidden: { opacity: 0, x: -2 },
+    visible: {
+      opacity: 1,
+      x: 0,
+      transition: {
+        duration: 0.1,
+        delayChildren: 0.3,
+        staggerChildren: 0.2,
+      },
+    },
+  };
+
+  const navLinksVariant = {
+    hidden: { opacity: 0, y: -10 },
+    visible: {
+      opacity: 1,
+      y: 0,
+    },
+  };
+
   return (
     <nav className="flex flex-row w-full gap-3 items-center justify-between text-xs mb-5">
       <div className="flex flex-col gap-1 items-start justify-start">
-        <div className="w-full flex flex-row justify-between items-start">
+        <motion.div
+          initial={{ opacity: 0, x: -10 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.1, delay: 0.1 }}
+          className="w-full flex flex-row justify-between items-start"
+        >
           <span className="font-black">I&apos;m Socially</span>
-        </div>
+        </motion.div>
         {socialLinks && socialLinks.length > 0 && (
-          <div className="flex flex-row justify-start items-center gap-2">
+          <motion.ul
+            variants={navBarVariant}
+            initial="hidden"
+            animate="visible"
+            className="flex flex-row justify-start items-center gap-2 list-none"
+          >
             {socialLinks.map((socialLink, index) => {
               const { title, url, icon } = socialLink;
               const SocialIcon = FontAwesome6Icon[icon];
@@ -27,26 +60,33 @@ const TopNav = () => {
                     key={index}
                     href={url}
                     target="_blank"
-                    className="bg-base-100 p-3 text-base-content shadow hover:text-primary hover:-translate-x-px hover:-translate-y-px hover:shadow-lg rounded-sm transition-all ease-in-out duration-300"
                     title={title}
                     onMouseOver={() => setActiveSocialLink(title)}
                     onMouseOut={() => setActiveSocialLink("Available")}
                   >
-                    <SocialIcon />
+                    <motion.li
+                      variants={navLinksVariant}
+                      className="bg-base-100 p-3 text-base-content shadow hover:text-primary hover:-translate-x-px hover:-translate-y-px hover:shadow-lg rounded-sm transition-all ease-in-out duration-300"
+                    >
+                      <SocialIcon />
+                    </motion.li>
                   </Link>
                 );
               }
             })}
-          </div>
+          </motion.ul>
         )}
         <div className="w-full flex flex-row justify-end items-start">
-          <span
+          <motion.span
+            initial={{ opacity: 0, x: 30 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.2, delay: 0.2 }}
             className={`font-black ${
               activeSocialLink === "Available" ? "" : "text-primary"
             }`}
           >
             {activeSocialLink}
-          </span>
+          </motion.span>
         </div>
       </div>
       <div className="hidden lg:flex flex-row gap-3 items-center justify-end">
